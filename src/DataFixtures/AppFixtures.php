aef50a7ec79e0ce631b0e0cf7b212e4cb5f857a6
Kevin NADIN <k.nadin@prismamedia.com>
2022-10-23 17:49:58 +0200

    WIP

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,11 +3,10 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(\Doctrine\Persistence\ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
